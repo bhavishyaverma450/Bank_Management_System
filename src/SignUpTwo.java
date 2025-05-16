2f31095a5c36618fc98060abe41fbec9a9c28696
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-05-16 14:14:20 +0530

    Add files via upload

---
+++ src/SignUpTwo.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SignUpTwo extends JFrame {
+
+    SignUpTwo() {
+
+        setLayout(null);
+        
+
+        setVisible(true);
+        getContentPane().setBackground(Color.WHITE);
+        setSize(850,800);
+        setLocationRelativeTo(null);
+
+
+    }
+
+    public static void main(String[] args) {
+        new SignUpTwo();
+    }
+}
